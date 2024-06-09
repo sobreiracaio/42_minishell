b27206fbf7e2b43ab3c26cd262dcee5600573987
sobreiracaio <caiosobreira@hotmail.com>
2024-06-09 21:06:17 +0100

    mais refatoracao cd

--- src/built_ins/exit.c
+++ src/built_ins/exit.c
@@ -6,44 +6,31 @@
 /*   By: crocha-s <crocha-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 19:39:14 by crocha-s          #+#    #+#             */
-/*   Updated: 2024/06/08 19:39:16 by crocha-s         ###   ########.fr       */
+/*   Updated: 2024/06/09 21:05:51 by crocha-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static bool	ft_isnumber(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str[i])
-		return (false);
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i++]))
-			return (false);
-	}
-	return (true);
-}
-
-static bool	islonglong(char *str)
+static bool	check_exit_code(char *str)
 {
 	int	is_negative;
 	int	len;
 
-	if (*str == '-')
+	is_negative = 0;
+	while (*str == '+' || *str == '-' || *str == ' ')
 	{
-		is_negative = 1;
+		if (*str == '-')
+			is_negative = 1;
 		str++;
 	}
-	while (*str == '0' || *str == ' ')
+	while (*str == '0')
 		str++;
-	if (!ft_isnumber(str))
+	if (!ft_isdigit(*str))
 		return (false);
-	len = ft_strlen(str);
+	while (*str && ft_isdigit(*str))
+		str++;
+	len = str - str;
 	if (len > 19)
 		return (false);
 	if (is_negative)
@@ -61,7 +48,7 @@ void	ms_exit(t_shell *shell, t_exec *cmd)
 		print_error(shell, cmd->argv[0], "too many arguments", 2);
 	if (cmd->argv[1])
 	{
-		if (!islonglong(cmd->argv[1]) && ft_strcmp(cmd->argv[1], "0"))
+		if (!check_exit_code(cmd->argv[1]) && ft_strcmp(cmd->argv[1], "0"))
 		{
 			err = ft_strjoin(cmd->argv[1], ": numeric argument required");
 			print_error(shell, cmd->argv[0], err, 2);
